fd51d193577b0801948649b9f7eb1f5d1c1a89ad
Fabian Arevalo Roa <arevalo.cpweb@gmail.com>
2022-10-05 21:52:45 +0100

    Feature: Se creo la carpeta QrTarjeta y el fichero QrTarjeta.jsx

---
+++ components/QrTarjeta/QrTarjeta.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, View} from "react-native";
+
+export default function QrTarjeta(){
+
+    return (
+    
+        <View> 
+        
+        </View>
+    
+    )
+
+}
+
+const styles = StyleSheet.create({
+
+
+
+});
