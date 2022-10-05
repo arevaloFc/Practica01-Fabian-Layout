8b9db9dd826e24b64423404786a13fd7c942476b
Fabian Arevalo Roa <arevalo.cpweb@gmail.com>
2022-10-05 20:27:14 +0100

    Feature: Se creo la carpeta MensajeTarjeta y el fichero MensajeTarjeta.jsx

---
+++ components/MensajeTarjeta/MensajeTarjeta.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, View} from "react-native"
+
+export default function MensajeTarjeta(){
+
+    return (
+        <View style={styles.mensajeTamanio}>
+            
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+
+
+});
